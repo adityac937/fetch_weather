e5e6bea069debde3d19d6b39394a27d844148377
aditya choudhary <adityac937@gmail.com>
2020-06-04 13:31:40 +0530

    Setup app  for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -26,7 +26,7 @@ const location = search.value
 messageOne.textContent='Hang On...'
 messageTwo.textContent=''
 
-fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+fetch('/weather?address=' + location).then((response) => {
 response.json().then((data) => {
 if (data.error) 
 {
